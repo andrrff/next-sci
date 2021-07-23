8baed48370104fbbd1424f90247e86d09af4a087
Suco de Fruta <andrecastanhal@gmail.com>
2021-07-23 20:59:33 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use actix_web::{
+    error, get, post, web, guard, middleware, App, Error, HttpRequest, HttpResponse,
+    HttpServer, Result,Responder
+};
+use std::{env, io};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
