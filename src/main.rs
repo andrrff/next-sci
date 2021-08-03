005808688ed3b2383e63a8e5c8fe16168317b531
Suco de Fruta <andrecastanhal@gmail.com>
2021-08-03 15:35:11 -0300

    test static files

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,13 @@
-use actix_web::{get, web, App, Error, HttpResponse, HttpServer, Responder};
-
-// #[get("/")]
-async fn index() -> impl Responder
-{
-    "Ola mundo".to_owned()
-}
-
-fn config(cfg: &mut web::ServiceConfig)
-{
-    cfg.service(
-        web::resource("/app")
-            .route(web::get().to(|| HttpResponse::Ok().body("/app")))
-            .route(web::head().to(|| HttpResponse::MethodNotAllowed()))
-    );
-
-    cfg.service(
-        web::resource("/api")
-            .route(web::get().to(|| HttpResponse::Ok().body("/api")))
-    );
-}
+use actix_files as fs;
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()>
-{
-    HttpServer::new(|| 
-    {
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
         App::new()
-            .service(
-                web::scope("/")
-                    .configure(config)
-                    .route("/welcome", web::get().to(index))
-            )
-            // .configure(f: F)
+            .service(fs::Files::new("/", "./static"))
     })
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
