603bf849bd28d8bd54c621935991d448ddd2116e
Suco de Fruta <andrecastanhal@gmail.com>
2021-08-04 16:31:43 -0300

    test math

--- src/main.rs
+++ src/main.rs
@@ -12,24 +12,36 @@ fn fresnel_C(t: f64, error: f64) -> Result<f64, String> {
     integrate(0.0, t, |x: f64| {x.powi(2) / (x.powi(2) - 5f64 * x + 6f64)}, error)
 }
 
-fn matching(value: String)
+fn matching(value: String) -> Vec<String>
 {
-    println!("{:?}", value.split_inclusive('^').collect::<Vec<_>>());
-    // for (i, j) in value.chars().enumerate()
-    // {
-    //     if j == '^'
-    //     {
-    //         println!("{:?}", value.chars().collect::<Vec<char>>()[i + 1])
-    //     }
-    //     // println!("{:?}", j);
-    // }
-    // println!("{:?}", value.chars().collect::<Vec<char>>());
+    // println!("{:?}", value.split_inclusive('^').collect::<Vec<&str>>().iter().map(|x: &&str| {
+    //     x.split_inclusive("+");
+    //     x.split_inclusive("-");
+    // }).collect::<Vec<&str>>());
+    let mut arguments: Vec<String> = Vec::new();
+    for (i, j) in value.chars().enumerate()
+    {
+        if j == '('
+        {
+            let mut iter = i;
+            let mut words: Vec<char> = Vec::new();
+            words.push(value.chars().collect::<Vec<_>>()[i]);
+            while value.chars().collect::<Vec<_>>()[iter] != ')'
+            {
+                words.push(value.chars().collect::<Vec<_>>()[iter + 1]);
+                iter += 1;
+            }
+            arguments.push(words.into_iter().collect())
+        }
+    }
+    arguments
 }
 
 // #[actix_web::main]
 fn main() {
     println!("{:?}", fresnel_C(5f64, 100000f64));
-    matching("10^2^3".to_owned());
+    // matching("10^2^3 + 4".to_owned());
+    println!("{:?}", matching("(10) + (10) + (10 + (-10) + 10)".to_owned()))
     // HttpServer::new(|| {
     //     App::new()
     //         .service(fs::Files::new("/", "./static"))
