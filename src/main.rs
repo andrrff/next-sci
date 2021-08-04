efb9642f9ecee9e9c8d3d458ffc76b69d7132b93
Suco de Fruta <andrecastanhal@gmail.com>
2021-08-04 16:59:51 -0300

    test math

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use actix_files as fs;
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, web, HttpResponse, Responder, get};
 
 use bacon_sci::integrate::integrate;
-use nalgebra::{VectorN, U1};
 
 fn fresnel_S(t: f64, error: f64) -> Result<f64, String> {
     integrate(0.0, t, |x: f64| x.powi(2).sin(), error)
@@ -12,41 +11,27 @@ fn fresnel_C(t: f64, error: f64) -> Result<f64, String> {
     integrate(0.0, t, |x: f64| {x.powi(2) / (x.powi(2) - 5f64 * x + 6f64)}, error)
 }
 
-fn matching(value: String) -> Vec<String>
-{
-    // println!("{:?}", value.split_inclusive('^').collect::<Vec<&str>>().iter().map(|x: &&str| {
-    //     x.split_inclusive("+");
-    //     x.split_inclusive("-");
-    // }).collect::<Vec<&str>>());
-    let mut arguments: Vec<String> = Vec::new();
-    for (i, j) in value.chars().enumerate()
-    {
-        if j == '('
-        {
-            let mut iter = i;
-            let mut words: Vec<char> = Vec::new();
-            words.push(value.chars().collect::<Vec<_>>()[i]);
-            while value.chars().collect::<Vec<_>>()[iter] != ')'
-            {
-                words.push(value.chars().collect::<Vec<_>>()[iter + 1]);
-                iter += 1;
-            }
-            arguments.push(words.into_iter().collect())
-        }
-    }
-    arguments
+// #[get("math")]
+// async fn math() -> String
+// {
+//     "fresnel_C(5f64, 100000f64).unwrap().to_string()".to_owned()
+// }
+
+#[get("/math")]
+async fn math() -> impl Responder {
+    HttpResponse::Ok().body(fresnel_C(5f64, 100000f64).unwrap().to_string())
 }
 
-// #[actix_web::main]
-fn main() {
-    println!("{:?}", fresnel_C(5f64, 100000f64));
-    // matching("10^2^3 + 4".to_owned());
-    println!("{:?}", matching("(10) + (10) + (10 + (-10) + 10)".to_owned()))
-    // HttpServer::new(|| {
-    //     App::new()
-    //         .service(fs::Files::new("/", "./static"))
-    // })
-    // .bind("127.0.0.1:8080")?
-    // .run()
-    // .await
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // println!("{:?}", fresnel_C(5f64, 100000f64).unwrap());
+    HttpServer::new(|| {
+        App::new()
+            .service(fs::Files::new("/", "./static"))
+            // .service(math)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
