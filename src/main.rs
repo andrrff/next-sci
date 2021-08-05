d924bc7ebadfaa8af3e84c52b89e71d984ebdd2a
Suco de Fruta <andrecastanhal@gmail.com>
2021-08-05 01:03:58 -0300

    test

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use actix_web::{App, HttpServer, web, HttpResponse, Responder, get};
 use bacon_sci::integrate::integrate;
 
 fn fresnel_S(t: f64, error: f64) -> Result<f64, String> {
-    integrate(0.0, t, |x: f64| x.powi(2).sin(), error)
+    integrate(0.0, t, |x: f64| x.sqrt(), error)
 }
 
 fn fresnel_C(t: f64, error: f64) -> Result<f64, String> {
@@ -13,14 +13,16 @@ fn fresnel_C(t: f64, error: f64) -> Result<f64, String> {
 
 #[get("/math")]
 async fn math() -> impl Responder {
-    HttpResponse::Ok().body(fresnel_C(5f64, 100000f64).unwrap().to_string())
+    HttpResponse::Ok().body(format!("<h1>∫²₀ x² / (x² - 5x + 6)<br></h1>{:?}", fresnel_C(5f64, 100000f64).unwrap().to_string()))
 }
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("{:?}", fresnel_S(2f64, 100f64));
     HttpServer::new(|| {
         App::new()
+            .service(math)
             .service(fs::Files::new("/", "./static/index/dist").index_file("index.html"))
     })
     .bind("127.0.0.1:8080")?
